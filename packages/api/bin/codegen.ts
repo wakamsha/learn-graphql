83d4ee8fa0b9a115041df56c04f7488807d31f92
wakamsha <wakamsha@gmail.com>
2023-04-09 16:18:01 +0900

    :construction: `packages/api` を実装する

---
+++ packages/api/bin/codegen.ts
@@ -0,0 +1,32 @@
+import { CodegenConfig } from '@graphql-codegen/cli';
+import { resolve } from 'path';
+
+/**
+ * GraphQL のスキーマ定義とオペレーション定義から
+ * TypeScript の型定義と `DocumentNode` オブジェクトを生成します。
+ *
+ * @see {@link https://the-guild.dev/graphql/codegen/plugins/presets/preset-client}
+ */
+const config: CodegenConfig = {
+  schema: ['schemas/*.graphql'],
+  documents: [resolve(__dirname, '../../../**/*.operation.graphql')],
+  ignoreNoDocuments: true,
+  generates: {
+    'src/generated/': {
+      preset: 'client',
+      config: {
+        enumsAsTypes: true,
+        strictScalars: true,
+        defaultScalarType: 'unknown',
+        useTypeImports: true,
+        nonOptionalTypename: true,
+        scalars: {
+          Date: 'Date',
+        },
+      },
+    },
+  },
+  hooks: { afterOneFileWrite: ['prettier -w'] },
+};
+
+export default config;
