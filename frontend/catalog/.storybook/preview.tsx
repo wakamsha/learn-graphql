c3b7c670c81af1545b72579eac18b1ed2287599c
wakamsha <wakamsha@gmail.com>
2023-04-13 11:04:33 +0900

    :construction: storybook が動作する状態を保つ

--- frontend/catalog/.storybook/preview.tsx
+++ frontend/catalog/.storybook/preview.tsx
@@ -1,7 +1,7 @@
-import { useClient } from '@learn-graphql/api/hooks/useClient';
-import { startMsw } from '@learn-graphql/core/utils/Catalog';
-import { type Args, type DecoratorFunction } from '@storybook/csf';
-import { type ReactFramework } from '@storybook/react';
+// import { useClient } from '@learn-graphql/api/hooks/useClient';
+// import { startMsw } from '@learn-graphql/core/utils/Catalog';
+// import { type Args, type DecoratorFunction } from '@storybook/csf';
+// import { type ReactFramework } from '@storybook/react';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -13,16 +13,16 @@ export const parameters = {
   },
 };
 
-export const decorators: Array<DecoratorFunction<ReactFramework & { canvasElement: unknown }, Args>> = [
-  (Story) => {
-    const { Provider, client } = useClient('http://localhost:4000/graphql');
+// export const decorators: Array<DecoratorFunction<ReactFramework & { canvasElement: unknown }, Args>> = [
+//   (Story) => {
+//     const { Provider, client } = useClient('http://localhost:4000/graphql');
 
-    return (
-      <Provider value={client}>
-        <Story />
-      </Provider>
-    );
-  },
-];
+//     return (
+//       <Provider value={client}>
+//         <Story />
+//       </Provider>
+//     );
+//   },
+// ];
 
-await startMsw();
+// await startMsw();
